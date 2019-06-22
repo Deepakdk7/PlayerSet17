4bb76c3994a6ba8aa301a75b622940f73fdd55ee
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-22 14:08:01 +0530

    Create 164.py

---
+++ 164.py
@@ -0,0 +1,12 @@
+def A(k):
+    for i in a:
+        if k==i:
+            print(k)
+            break
+    else:
+        k=k-1
+        A(k)
+ax=list(map(int,input().split()))
+a=list(map(int,input().split()))
+k=ax[1]
+A(k)
